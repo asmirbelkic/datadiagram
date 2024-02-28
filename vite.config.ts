7906221f81c7b08097bac7b8d00df79659c0d437
Asmir Belkic <asmir.belkic@gmail.com>
2024-02-28 16:59:16 +0100

    Mise à jour de la configuration Svelte et Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import legacy from "@vitejs/plugin-legacy";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		legacy({ 'node_modules/leader-line-new/leader-line.min.js': 'LeaderLine' } as Record<string, string>),
 	 ],
 });
