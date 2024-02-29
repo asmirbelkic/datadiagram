71c759aa6228c037d8eda4b273141b5ea9f30aea
Asmir Belkic <asmir.belkic@gmail.com>
2024-02-29 12:31:17 +0100

    Mise à jour de la fonction de déplacement de la fenêtre

--- src/routes/Window.svelte
+++ src/routes/Window.svelte
@@ -17,13 +17,7 @@
 
 	$: grabbinStyle = moving ? `transform: scale(1.03) rotate(${parseFloat((Math.random() * (3 - -3) + -3).toString()).toFixed(2)}deg)` : "";
 	const onMouseDown = (event: MouseEvent) => {
-		event.preventDefault(); // Empêche le comportement par défaut du navigateur
 		moving = true;
-
-		// Obtenez la position de l'élément par rapport au viewport
-		const rect = mainElement.getBoundingClientRect();
-		console.log(rect.x, rect.y);
-		console.log(event.clientX, event.clientY);
 
 		// Enregistrez la position initiale du curseur par rapport à l'élément.
 		x = event.clientX;
@@ -32,11 +26,14 @@
 
 	const onMouseMove = (event: MouseEvent) => {
 		if (moving) {
-			const dx = event.clientX - x;
-			const dy = event.clientY - y;
+			let dx = event.clientX - x;
+			let dy = event.clientY - y;
 
-			mainElement.style.top = mainElement.offsetTop + dy + "px";
-			mainElement.style.left = mainElement.offsetLeft + dx + "px";
+			const scaleFactor = 1.01;
+			dx /= scaleFactor;
+			dy /= scaleFactor;
+			// mainElement.style.top = mainElement.offsetTop + dy + "px";
+			// mainElement.style.left = mainElement.offsetLeft + dx + "px";
 
 			x = event.clientX;
 			y = event.clientY;
@@ -48,29 +45,16 @@
 				return updatedPositions;
 			});
 
-			position = [mainElement.offsetLeft, mainElement.offsetTop];
+			position = [mainElement.offsetLeft + dx, mainElement.offsetTop + dy];
 		}
 	};
 
-	function handleClick() {
-		$isSelecting = !$isSelecting;
-		if ($isSelecting) {
-			selectedElement.set({
-				name: name,
-				element: null,
-				id: null,
-				table: name,
-			});
-		} else {
-			selectedElement.set({
-				name: null,
-				element: null,
-				id: null,
-			});
-		}
+	function onMouseUp() {
+		moving = false;
+
+		lines.forEach(line => line.position());
 	}
 
-	let selectedFullName = $selectedElement.table;
 	function toggleReadOnly() {
 		if (!editName) readOnly = !readOnly;
 	}
@@ -96,7 +80,7 @@
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <!-- svelte-ignore a11y-mouse-events-have-key-events -->
 <!-- <main  bind:this={mainElement} on:click|stopPropagation={(event) => handleElementClick(event, name)} on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave} class:selected={name === selectedFullName} class:unhovered={$isSelecting && name === selectedFullName} class:hovered={$isSelecting && $hoveredElement.table === name && selectedFullName !== name} class:grabbing={moving} class="outer" style="left: {position[0]}px; top: {position[1]}px; {grabbinStyle}" use:clickOutside={handleOutsideClick}> -->
-<main bind:this={mainElement} on:click|stopPropagation={(event) => handleElementClick(event, name)} on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave} class:grabbing={moving} class="outer" style="left: {position[0]}px; top: {position[1]}px; {grabbinStyle}" use:clickOutside={handleOutsideClick}>
+<main bind:this={mainElement} on:click|stopPropagation={(event) => handleElementClick(event, name)} on:mouseup={onMouseUp} on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave} class:grabbing={moving} class="outer" style="left: {position[0]}px; top: {position[1]}px; {grabbinStyle}" use:clickOutside={handleOutsideClick}>
 	<!-- <main bind:this={mainElement} class:grabbing={moving} class="outer" style="left: {position[0]}px; top: {position[1]}px; {grabbinStyle}" use:clickOutside={handleOutsideClick}> -->
 	<header>
 		<h5 on:dblclick={() => (editName = true)}>
@@ -130,18 +114,12 @@
 			</svg>
 		</button>
 		<!-- svelte-ignore a11y-no-static-element-interactions -->
-		<span class="btn" on:click|stopPropagation={toggleReadOnly}>
+		<button class="btn" on:click|stopPropagation={toggleReadOnly}>
 			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3">
 				<path d="M12 20h9"></path>
 				<path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
 			</svg>
-		</span>
-		<!-- <span class="btn" on:click|stopPropagation={handleClick}>
-			<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path d="M16 17L22 11L16 5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-				<path d="M3.91164 19.3885C4.29807 17.7083 5.04197 16.1312 6.09271 14.7644C7.14345 13.3976 8.47636 12.2733 10.0007 11.468C11.525 10.6626 13.205 10.1952 14.9262 10.0975C16.6474 9.99977 18.3695 10.2741 19.9752 10.9017" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-			</svg>
-		</span> -->
+		</button>
 	</header>
 	<slot />
 </main>
